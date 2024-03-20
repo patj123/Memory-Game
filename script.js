01118c232716e9e06f1159ce578b3863f827eea0
Patrick Gibbs <patrickjkgibbs@gmail.com>
2024-03-20 16:41:26 -0700

    Added local storage to it. Reorganized the code to make JS more readable using AI

--- script.js
+++ script.js
@@ -1,108 +1,72 @@
+// Get the game container element
 const gameContainer = document.getElementById("game");
 
+// Initialize variables for tracking clicked cards, current score, and game state
 let cardOne = null;
-
 let cardTwo = null;
-
 let noClicking = false;
-
 let currentScore = 0;
-
-
-
-
-//keeping track of hhow
 let cardsFlipped = 0;
 
+// Get the restart button element and create a restart button
+let restart = document.getElementById('restart');
+let restartButton = document.createElement('button');
+restartButton.innerText = "Restart";
+
+// Define an array of colors for the cards
 const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple"
+  "red", "blue", "green", "orange", "purple",
+  "red", "blue", "green", "orange", "purple"
 ];
 
-// here is a helper function to shuffle an array
-// it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want ot research more
+// Shuffle the array of colors
+let shuffledColors = shuffle(COLORS);
+
+// Function to shuffle an array (Fisher-Yates algorithm)
 function shuffle(array) {
   let counter = array.length;
-
-  // While there are elements in the array
   while (counter > 0) {
-    // Pick a random index
     let index = Math.floor(Math.random() * counter);
-
-    // Decrease counter by 1
     counter--;
-
-    // And swap the last element with it
     let temp = array[counter];
     array[counter] = array[index];
     array[index] = temp;
   }
-
   return array;
 }
 
-let shuffledColors = shuffle(COLORS);
-
-// this function loops over the array of colors
-// it creates a new div and gives it a class with the value of the color
-// it also adds an event listener for a click for each card
+// Function to create divs for each color and set up event listeners
 function createDivsForColors(colorArray) {
   for (let color of colorArray) {
-    // create a new div
     const newDiv = document.createElement("div");
-
-    // give it a class attribute for the value we are looping over
     newDiv.classList.add(color);
-
-    // call a function handleCardClick when a div is clicked on
     newDiv.addEventListener("click", handleCardClick);
-
-    // append the div to the element with an id of game
     gameContainer.append(newDiv);
   }
 }
 
-
-
-
-
-
-// TODO: Implement this function!
-function handleCardClick(event) {
-  console.log(noClicking);
-  console.log(cardsFlipped)
-  if (noClicking) return;
-  if (event.target.classList.contains("flipped")) {
-    return;
+// Function to remove all card divs from the game container
+function removeDivsForColors() {
+  while (gameContainer.firstChild) {
+    gameContainer.removeChild(gameContainer.firstChild);
   }
-  //variable assigns what color is clicked on
+}
+
+// Function to handle card clicks
+function handleCardClick(event) {
+  if (noClicking || event.target.classList.contains("flipped")) return;
+
   let currentCard = event.target;
-
-  //add boolean if card is clicked on
   currentCard.style.backgroundColor = currentCard.classList[0];
-
   if (!cardOne || !cardTwo) {
-
     if (!currentCard.classList.add('flipped')) {
-      setScore(currentScore + 1)
+      setScore(currentScore + 1);
     }
     cardOne = cardOne || currentCard;
     cardTwo = currentCard === cardOne ? null : currentCard;
-
   }
-  //console.log(currentCard.classList[0])
-  //cards stay flipped if matched
+
   if (cardOne && cardTwo) {
-    //console.log(cardOne.className);
     noClicking = true;
     let gif1 = cardOne.className;
     let gif2 = cardTwo.className;
@@ -111,13 +75,12 @@ function handleCardClick(event) {
       cardTwo.removeEventListener('click', handleCardClick);
       cardOne = null;
       cardTwo = null;
-      noClicking = false
-      cardsFlipped += 2
-    } else { //switching card back after two seconds if cards do not match
+      noClicking = false;
+      cardsFlipped += 2;
+    } else {
       setTimeout(function () {
         cardOne.classList.remove("flipped");
         cardTwo.classList.remove("flipped");
-        console.log(cardOne);
         cardOne.style.backgroundColor = "";
         cardTwo.style.backgroundColor = "";
         cardOne = null;
@@ -126,73 +89,71 @@ function handleCardClick(event) {
       }, 1000);
     }
   }
-  //restart button appears when all colors are matched
+
   if (cardsFlipped === COLORS.length && cardsFlipped > 0) {
-    //getting #restart from html
-    let restart = document.getElementById('restart');
-    //creating button
-    let restartButton = document.createElement('button');
-    //what the same of the botton the user will see
-    restartButton.innerText = "Restart";
-    //clears previous content
-    restart.innerHTML = ''
-    alert('Game Over your score is ' + currentScore)
-    //connected HTML ID element to new button
-    restart = restart.appendChild(restartButton)
-    //completed button that restarts the game
-    restart.addEventListener('click', function (event) {
-      //this is how game reloads
-      location.reload(true)
-    })
+    restart.innerHTML = '';
+    lowestScore();
+    restart = restart.appendChild(restartButton);
   }
-  // you can use event.target to see which element was clicked
-  console.log("you just clicked", event.target);
 }
 
+// Function to restart the game
+restart.addEventListener('click', function (event) {
+  setScore(0);
+  cardsFlipped = 0;
+  removeDivsForColors();
+  initializeGame();
+  displayLowestScore();
+});
+
+// Function to set the current score
 function setScore(newScore) {
   currentScore = newScore;
   document.getElementById("current-score").innerText = currentScore;
-
 }
 
-//Start of Menu code
+// Function to update or set the lowest score in localStorage
+function lowestScore() {
+  if (localStorage.getItem('lowestScore') === null) {
+    localStorage.setItem('lowestScore', currentScore);
+  } else if (currentScore < localStorage.getItem('lowestScore')) {
+    localStorage.setItem('lowestScore', currentScore);
+  }
+}
 
-//getting menu div node from html
+// Function to display the lowest score
+function displayLowestScore() {
+  let lowestScore = localStorage.getItem('lowestScore');
+  document.getElementById("lowest-score").innerText = lowestScore;
+}
+
+// Function to initialize the game
+function initializeGame() {
+  createDivsForColors(shuffle(COLORS));
+}
+
+// Start of Menu code
+
+// Get menu screen and start button elements
 const menuScreen = document.getElementById("menu-screen");
-
-// getting start button div node from html
 const start = document.getElementById("start");
 
+// Create a start button
 const startButton = document.createElement('button');
-
-//defining start button
-
 startButton.innerText = 'Start Game';
+startButton.addEventListener('click', startGame);
 
-//adding function to button
+// Get game screen element
+const gameScreen = document.getElementById("game-screen");
 
-startButton.addEventListener('click', startGame)
+// Add start button to the menu
+start.appendChild(startButton);
 
-//getting game screen div node from html
-
-const gameScreen = document.getElementById("game-screen")
-
-// Adding button to menu
-
-start.appendChild(startButton)
-
+// Function to start the game
 function startGame() {
   menuScreen.style.display = 'none';
   gameScreen.style.display = "block";
-  initializeGame()
+  displayLowestScore();
+  initializeGame();
 }
-
-
-//end of menu code
-
-
-
-function initializeGame() {
-  //when DOM loads
-  createDivsForColors(shuffle(COLORS))
-}
+// End of Menu code
